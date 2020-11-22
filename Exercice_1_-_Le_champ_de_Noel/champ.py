c900bc8a9235d6bf351e12ab0497568ebe138305
gregH <mail.greg.henry@gmail.com>
2020-11-22 19:19:57 +0100

    Initial setup for the solution of the 1st exercize
    + setup constants
    + prepare print
    + list of TODOs

--- Exercice_1_-_Le_champ_de_Noel/champ.py
+++ Exercice_1_-_Le_champ_de_Noel/champ.py
@@ -1,1 +1,31 @@
-# Met ton code ici
+"""
+Xmas tree field.
+"""
+
+
+def main():
+    """main"""
+    # initialization
+    width = 20
+    height = 20
+    tree = ["^", "^ ^", "(o  )", "(o  o )", "U"]
+    # create field with borders
+    field = [
+        ["#" if (j in (0, width - 1) or i in (0, height - 1)) else " " for j in range(width)]
+        for i in range(height)
+    ]
+
+    # TODO:
+    # + get a single random place
+    # + place only tree trunks
+    # + place trees wholy
+    # + get several random places
+    # + do perspective (sort by height?)
+
+    # show the world your Xmas tree field
+    for line in field:
+        print("".join(line))
+
+
+if __name__ == "__main__":
+    main()
